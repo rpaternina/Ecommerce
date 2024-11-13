50878919dfa8ef4274561fde4eb7d00ee6e9c61b
rpaternina <robertpaternina65@gmail.com>
2024-11-13 15:25:21 -0500

    Model- User listo

---
+++ microservicio-api-gateway/src/main/java/com/robert/microservicio_api_gateway/model/Role.java
@@ -0,0 +1,7 @@
+package com.robert.microservicio_api_gateway.model;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
--- microservicio-api-gateway/src/main/java/com/robert/microservicio_api_gateway/model/User.java
+++ microservicio-api-gateway/src/main/java/com/robert/microservicio_api_gateway/model/User.java
@@ -1,4 +1,37 @@
 package com.robert.microservicio_api_gateway.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", length = 100, unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "paswword", nullable = false)
+    private String password;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "fecha_creacion", nullable = false)
+    private LocalDateTime fechaCreacion;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
 }
